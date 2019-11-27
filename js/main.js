756a5931627febca32b1474fb210da1ac5d44285
SonYUA <pankivluck@gmail.com>
2019-11-27 19:48:31 +0200

    five

--- js/main.js
+++ js/main.js
@@ -11,3 +11,26 @@ function showMenu() {
 }
 burgerIcon.addEventListener('click', showMenu);
 closeIcon.addEventListener('click', showMenu);
+
+/*ANCHOR */
+
+$(document).ready(function() {
+    $("#menu").on("click", "a", function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
+
+/* ANIMATION SCROLL */
+
+$(window).scroll(function() {
+    $('.movLeft').each(function() {
+        var imagePos = $(this).offset().top;
+        var topOfWindow = $(window).scrollTop();
+        if (imagePos < topOfWindow + 600) {
+            $(this).addClass('slideInLeft ');
+        }
+    });
+});
